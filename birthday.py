22b2ef13b2621a8e84ddbc87ce322de291ffdfa9
Spyglass117 <46973734+Spyglass117@users.noreply.github.com>
2019-02-01 17:21:06 -0500

    Updated from Brython Server: 2/1/2019 5:21:05 PM

--- birthday.py
+++ birthday.py
@@ -31,3 +31,17 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+from datetime import datetime
+from calendar import month_name
+
+name = input("Hello, what is your name?")
+birthmonth = input("Hi {0}, what was the name of the month you were born in?".format(name))
+birthyear = input("And what year were you born in, {0}?".format(name))
+birthday = input("And the day?")
+
+month = datetime.today().month
+todayyear = datetime.today().year
+todayday = datetime.today().day
+
+todaymonth = month_name[month]
+
